b8d977898b7675b717ba0ec25af01039e40f3f15
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-15 14:36:44 -0600

    Commit 7 - put route working

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const Pizzas = require('./models/pizzas');
 const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
 app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
 
+
+app.put('/pizzas/:id', (req, res) => {
+	
+	Pizzas[req.params.id] = req.body;
+	res.redirect('/pizzas');
+
+});
 
 app.get('/pizzas', (req, res) => {
 	res.render('index.ejs', {
@@ -12,9 +21,12 @@ app.get('/pizzas', (req, res) => {
 	})
 });
 
+
 app.get('/pizzas/:id/edit', (req, res) => {
-	res.render('edit.ejs', {pizzas: Pizzas[req.params.id]});
+	res.render('edit.ejs', {pizzas: Pizzas[req.params.id], id: req.params.id});
 });
+
+
 
 app.delete('/pizzas/:id', (req, res) => {
 	Pizzas.splice(req.params.id, 1);
