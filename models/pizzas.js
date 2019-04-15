c1c06d842b0d21f7766e02f2f32c7f31ccd0dfdd
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-15 12:01:49 -0600

    Commit 2 - array set up

---
+++ models/pizzas.js
@@ -0,0 +1,17 @@
+const Pizzas = [
+	{
+		name: 'pepperoni',
+		isReady: true,
+		image: 'https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/image/recipes/ck/gluten-free-cookbook/pepperoni-pizza-ck-x.jpg?itok=NWreajsZ'
+	},
+	{
+		name: 'sausage',
+		isReady: true,
+		image: 'https://www.allfoodsrecipes.com/wp-content/uploads/2018/01/Turkey-Sausage-Pizza-500x375.jpg'
+	},
+	{
+		name: 'supreme',
+		isReady: false,
+		image: 'https://www.tasteofhome.com/wp-content/uploads/2017/10/exps25356_TH10603D13.jpg'
+	}
+]
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const pizzas = require('./pizzas');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
