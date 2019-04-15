770985f95711b9ef39d7400764291d95548afbc4
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-15 13:43:48 -0600

    Commit 4 - delete route working

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const Pizzas = require('./models/pizzas');
+const methodOverride = require('method-override');
 
-
+app.use(methodOverride('_method'));
 
 
 app.get('/pizzas', (req, res) => {
@@ -11,7 +12,13 @@ app.get('/pizzas', (req, res) => {
 	})
 });
 
+app.delete('/pizzas/:id', (req, res) => {
+	Pizzas.splice(req.params.id, 1);
+	res.redirect('/pizzas');
 
+
+	// res.send('delete route works');
+});
 
 
 
